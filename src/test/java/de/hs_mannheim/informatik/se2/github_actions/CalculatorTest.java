5e19c9329c2a57fb2efb12a89286259fe5c9ceca
oliverhummel <o.hummel@hs-mannheim.de>
2021-12-09 10:06:40 +0100

    Tests hinzugefügt

--- src/test/java/de/hs_mannheim/informatik/se2/github_actions/CalculatorTest.java
+++ src/test/java/de/hs_mannheim/informatik/se2/github_actions/CalculatorTest.java
@@ -6,9 +6,19 @@ import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
+//    @Test
+//    void test() {
+//        fail("Not yet implemented");
+//    }
+    
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void testAddition() {
+        Calculator cal = new Calculator();
+        
+        assertEquals(5, cal.add(5));
+        assertEquals(5, cal.add(3, 2));
+        assertEquals(-1, cal.add(-5, 1, 1, 2));
+        assertEquals(0, cal.add(0, 0, 1, -1));
     }
 
 }
