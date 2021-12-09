11289d42bc8881079c5ec8f10292390650d5a387
oliverhummel <o.hummel@hs-mannheim.de>
2021-12-09 09:47:07 +0100

    JUnit hinzugefügt

---
+++ src/test/java/de/hs_mannheim/informatik/se2/github_actions/CalculatorTest.java
@@ -0,0 +1,14 @@
+package de.hs_mannheim.informatik.se2.github_actions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
